fcfff8e0dcad5454a7c477f3c62289186b3ce969
AndrewF <andrew@sytrue.com>
2022-11-09 08:22:12 -0500

    Add Cronjob

--- ansible/playbooks/roles/nginx/files/cloudflare_ips.sh
+++ ansible/playbooks/roles/nginx/files/cloudflare_ips.sh
@@ -4,10 +4,6 @@ set -e
 
 cf_ips() {
 echo "# https://www.cloudflare.com/ips"
-echo "allow 70.161.198.2/32;"
-echo "allow 192.168.1.20/32;"
-echo "allow 192.168.1.1/32;"
-echo "allow 174.250.14.3/32;"
 
 for type in v4 v6; do
 echo "# IP$type"
@@ -15,6 +11,6 @@ curl -sL "https://www.cloudflare.com/ips-$type/" | sed "s|^|allow |g" | sed "s|\
 echo
 done
 
-echo "# Generated at $(LC_ALL=C date)"
+echo "#CF Update Generated at $(LC_ALL=C date)" > /tmp/cf.log
 }
 (cf_ips && echo "#deny all; # deny all remaining ips") > /etc/nginx/allow-cloudflare-only.conf
