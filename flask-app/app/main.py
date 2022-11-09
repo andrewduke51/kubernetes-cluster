4aa4206b39a95159399337e8450c2ef9813ea9b9
AndrewF <andrew@sytrue.com>
2022-11-08 21:57:52 -0500

    Add mongoDB

--- flask-app/app/main.py
+++ flask-app/app/main.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, jsonify, request
 from multiprocessing import Value
+from datetime import datetime
+import pymongo
 import socket
 import json
 import os
 
-# Connect to Redis
+# Variables ##
+app = Flask(__name__, template_folder='views')
 counter = Value('i', 0)
-app=Flask(__name__,template_folder='views')
+mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
+visitorsdb = mongoconnection["ips"]
+ipcollection = visitorsdb["visitors"]
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    captured = {
+    "time_stamp" : datetime.now(),
+    "ip_addresses" : request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    }
+    ipcollection.insert_one(captured)
     return render_template('index.html')
+
 
 @app.route("/clicknext")
 def home():
     with counter.get_lock():
         counter.value += 1
-    data=counter.value
-    ip_addr=request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    data = counter.value
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     return render_template('home.html',
                            title="page",
                            json_value=json.dumps(data),
@@ -25,9 +36,11 @@ def home():
                            hostname=socket.gethostname()
                            )
 
+
 @app.route('/proxy_client')
 def proxy_client():
     return render_template('proxy_client.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
