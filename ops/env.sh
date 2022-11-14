a36ffd43a9ab86b095358e1b826e874706089c99
AndrewF <andrew@sytrue.com>
2022-11-13 21:28:05 -0500

    Update pipeline

--- ops/env.sh
+++ ops/env.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-HELM_VERSION=3.7.2
-
-# install kubectl
-cd ops && curl -LO https://dl.k8s.io/release/v1.25.0/bin/linux/amd64/kubectl
-chmod +x kubectl
-
-## Install helm ##
-curl -LO "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -o helm-v${HELM_VERSION}-linux-amd64.tar.gz
-tar -zxvf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-mv linux-amd64/helm helm
-chmod +x helm
-
 # set config
 mkdir ../local_files
 echo "${CONFIG}" | base64 -d > config
