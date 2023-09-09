335744eb82d1e54db41b49f406c4b8912b069379
AndrewF <andrew@sytrue.com>
2023-09-09 10:50:10 -0400

    use countDocuments

--- flask-app/app/main.py
+++ flask-app/app/main.py
@@ -41,21 +41,21 @@ def blog2():
 @app.route("/clicknext")
 def home():
     captured = {
-        "time_stamp" : datetime.now(),
-        "ip_addresses" : request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+        "time_stamp": datetime.now(),
+        "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     }
     ipcollection.insert_one(captured)
-    with counter.get_lock():
-        counter.value += 1
-    data = counter.value
+
+    # Retrieve the current visitor count from the database
+    total_visitors = ipcollection.count_documents({})
+
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     return render_template('clicknext.html',
                            title="page",
-                           json_value=json.dumps(data),
+                           json_value=json.dumps(total_visitors),
                            ip_addr=ip_addr,
                            hostname=socket.gethostname()
                            )
-
 
 @app.route('/proxy_client')
 def proxy_client():
