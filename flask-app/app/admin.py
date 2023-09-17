23e5cdf5435b9f03e27791c5c46bd89ec4a290c9
AndrewF <andrew@sytrue.com>
2023-09-17 14:18:43 -0400

    simplify

--- flask-app/app/admin.py
+++ flask-app/app/admin.py
@@ -1,8 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, g
 from datetime import datetime
 import pymongo
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 # Create a Blueprint for the admin route
 admin_bp = Blueprint("admin_bp", __name__)
@@ -12,39 +10,35 @@ mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
 visitorsdb = mongoconnection["ips"]
 attackcollection = visitorsdb["attacks"]
 
-# Create a limiter instance with a rate limit of 3 attempts per minute for login
-limiter = Limiter(
-    key_func=get_remote_address,
-    storage_uri="memory://",  # You can choose a different storage option
-    default_limits=["3 per minute"]
-)
-
-# Initialize a dictionary to track login attempts
+# Create a counter to track login attempts
 login_attempts = {}
 
 @admin_bp.route('/admin', methods=["GET", "POST"])
 @admin_bp.route('/config', methods=["GET", "POST"])
-@limiter.request_filter
 def honeypot():
+    remote_address = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+
+    # Increment the login attempt count for this IP
+    if remote_address not in login_attempts:
+        login_attempts[remote_address] = 0
+    login_attempts[remote_address] += 1
+
+    # Check if the login attempts exceed the limit (3 attempts)
+    if login_attempts[remote_address] >= 3:
+        return "Oops! You stumbled into our honeypot. No secrets for you! 😄"
+
     if request.method == "POST":
-        # Increment the login attempt count for this IP
-        remote_address = get_remote_address()
-        login_attempts[remote_address] = login_attempts.get(remote_address, 0) + 1
-
-        # Check if the login attempts exceed the limit (3 attempts)
-        if login_attempts[remote_address] >= 3:
-            return "Oops! You stumbled into our honeypot. No secrets for you! 😄"
-
-    # Log and save attack details to the "attacks" collection
-    captured = {
-        "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
-        "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr),
-        "request_data": {
-            "path": request.path,
-            "method": request.method,
-            "user_agent": request.user_agent.string,
-            "data": request.get_data(as_text=True),
+        # Log and save attack details to the "attacks" collection
+        captured = {
+            "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
+            "ip_addresses": remote_address,
+            "request_data": {
+                "path": request.path,
+                "method": request.method,
+                "user_agent": request.user_agent.string,
+                "data": request.get_data(as_text=True),
+            }
         }
-    }
-    attackcollection.insert_one(captured)  # Use a separate collection for attacks
+        attackcollection.insert_one(captured)  # Use a separate collection for attacks
+
     return render_template('admin.html')
