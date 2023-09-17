3b61e54a1eeb25898a941b6990c86a6a67e1db18
AndrewF <andrew@sytrue.com>
2023-09-17 17:04:30 -0400

    Update redis module 5

--- flask-app/app/admin.py
+++ flask-app/app/admin.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import pymongo
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from shared import limiter, redis_server
 
 # Create a Blueprint for the admin route
 admin_bp = Blueprint("admin_bp", __name__)
@@ -14,46 +13,21 @@ mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
 visitorsdb = mongoconnection["ips"]
 attackcollection = visitorsdb["attacks"]
 
-# Create a Limiter instance with a custom rate limit function
-limiter = Limiter(
-    key_func=get_remote_address,
-    storage_uri=redis_server.connection_pool.connection_kwargs.get('uri')  # Use the connection_pool attribute
-)
-
-# Define a custom rate limit function that allows 3 tries per minute
-@limiter.request_filter
-def custom_rate_limit():
-    remote_address = get_remote_address()
-    attempts = limiter.limiter.get(remote_address)
-    print(f"Remote Address: {remote_address}, Attempts: {attempts}")
-
-    if attempts is not None and attempts >= 3:
-        return False  # Limit exceeded, deny the request
-    return True  # Limit not exceeded, allow the request
-
-# Apply the limiter to the admin Blueprint
-limiter.limit(custom_rate_limit)(admin_bp)
-
 @admin_bp.route('/admin', methods=["GET", "POST"])
 @admin_bp.route('/config', methods=["GET", "POST"])
 def honeypot():
-    if request.method == "POST":
-        # Check if the login attempts exceed the limit (3 attempts)
-        if not custom_rate_limit():
-            return "Oops! You stumbled into our honeypot. No secrets for you! 😄"
-
-        # Log and save attack details to the "attacks" collection
-        captured = {
-            "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
-            "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr),
-            "request_data": {
-                "path": request.path,
-                "method": request.method,
-                "user_agent": request.user_agent.string,
-                "data": request.get_data(as_text=True),
-            }
+    # Log and save attack details to the "attacks" collection
+    captured = {
+        "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
+        "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr),
+        "request_data": {
+            "path": request.path,
+            "method": request.method,
+            "user_agent": request.user_agent.string,
+            "data": request.get_data(as_text=True),
         }
-        attackcollection.insert_one(captured)  # Use a separate collection for attacks
+    }
+    attackcollection.insert_one(captured)  # Use a separate collection for attacks
 
     # Render the admin.html template
     return render_template('admin.html')
