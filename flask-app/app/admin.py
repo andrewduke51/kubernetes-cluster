c40d10bb6f3b61f1de7f9a06e3846dada2a52ad5
AndrewF <andrew@sytrue.com>
2023-09-17 12:03:59 -0400

    revert

--- flask-app/app/admin.py
+++ flask-app/app/admin.py
@@ -1,56 +1,20 @@
-# admin.py
-
-from flask import Blueprint, render_template, request, jsonify
+from flask import Flask, render_template, request, Blueprint
 from datetime import datetime
 import pymongo
-import socket
-import json
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
-# Create a Blueprint for the admin route
+# Create a Blueprint for the admin routes
 admin_bp = Blueprint("admin_bp", __name__)
 
-# Define the IP collection variable here
+# Define the attackcollection variable here
 mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
 visitorsdb = mongoconnection["ips"]
 ipcollection = visitorsdb["visitors"]
+attackcollection = visitorsdb["attacks"]
 
-# Create a limiter instance with a rate limit of 3 attempts per minute for login
-limiter = Limiter(
-    key_func=get_remote_address,
-    storage_uri="memory://",  # You can choose a different storage option
-    default_limits=["3 per minute"]
-)
-
-# Counter to track login attempts
-login_attempts = {}
-
-
+# Routes for /admin and /config
 @admin_bp.route('/admin', methods=["GET", "POST"])
 @admin_bp.route('/config', methods=["GET", "POST"])
-@limiter.request_filter
 def honeypot():
-    # Check if the subpath matches "/admin"
-    if request.method == "POST":
-        # Check login credentials here (you can implement your logic)
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        # Replace this with your actual login validation logic
-        valid_credentials = validate_login(username, password)
-
-        if valid_credentials:
-            return render_template('admin.html')
-        else:
-            # Increment the login attempt count for this IP
-            remote_address = get_remote_address()
-            login_attempts[remote_address] = login_attempts.get(remote_address, 0) + 1
-
-            # Check if the login attempts exceed the limit (3 attempts)
-            if login_attempts[remote_address] >= 3:
-                return "Oops! You stumbled into our honeypot. No secrets for you! 😄"
-
     # Log and save attack details to the "attacks" collection
     captured = {
         "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
