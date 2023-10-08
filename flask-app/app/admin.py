78473d2c2665f4daccfbb94a0fa91f0c50f4059a
AndrewF <andrew@sytrue.com>
2023-10-07 20:46:42 -0400

    test nelson

--- flask-app/app/admin.py
+++ flask-app/app/admin.py
@@ -10,13 +10,19 @@ mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
 visitorsdb = mongoconnection["ips"]
 attackcollection = visitorsdb["attacks"]
 
+# Dictionary to keep track of IP addresses and their request counts
+ip_request_counts = {}
+
 @admin_bp.route('/admin', methods=["GET", "POST"])
 @admin_bp.route('/config', methods=["GET", "POST"])
 def honeypot():
+    # Get the IP address of the requester
+    ip_address = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+
     # Log and save attack details to the "attacks" collection
     captured = {
         "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
-        "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr),
+        "ip_addresses": ip_address,
         "request_data": {
             "path": request.path,
             "method": request.method,
@@ -25,6 +31,14 @@ def honeypot():
         }
     }
     attackcollection.insert_one(captured)  # Use a separate collection for attacks
+
+    # Check if the IP address has made multiple requests
+    if ip_address in ip_request_counts:
+        ip_request_counts[ip_address] += 1
+        if ip_request_counts[ip_address] >= 3:
+            # After three requests, respond with a funny message
+            funny_response = "Ha-ha! You've stumbled into our honeypot! 😜"
+            return jsonify({"message": funny_response}), 200
 
     # Render the admin.html template
     return render_template('admin.html')
