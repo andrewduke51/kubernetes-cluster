dfbc52ad1ed2df9186cdd9781f881d8b82d5d3ef
AndrewF <andrew@sytrue.com>
2023-10-07 21:05:43 -0400

    test nelson 3

--- flask-app/app/admin.py
+++ flask-app/app/admin.py
@@ -10,21 +10,39 @@ mongoconnection = pymongo.MongoClient("mongodb://mongo-service.mongo:27017/")
 visitorsdb = mongoconnection["ips"]
 attackcollection = visitorsdb["attacks"]
 
+# Dictionary to keep track of IP addresses and their login attempts
+ip_login_attempts = {}
+
+# Maximum number of allowed login attempts before humorous response
+max_login_attempts = 3
+
 @admin_bp.route('/admin', methods=["GET", "POST"])
 @admin_bp.route('/config', methods=["GET", "POST"])
 def honeypot():
-    # Log and save attack details to the "attacks" collection
-    captured = {
-        "time_stamp": datetime.now().strftime("%m/%d/%y - %H:%M:%S"),
-        "ip_addresses": request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr),
-        "request_data": {
-            "path": request.path,
-            "method": request.method,
-            "user_agent": request.user_agent.string,
-            "data": request.get_data(as_text=True),
-        }
-    }
-    attackcollection.insert_one(captured)  # Use a separate collection for attacks
+    # Get the IP address of the requester
+    ip_address = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
 
-    # Render the admin.html template
-    return render_template('admin.html')
+    # Check if the IP address has exceeded the max_login_attempts
+    if ip_address in ip_login_attempts and ip_login_attempts[ip_address] >= max_login_attempts:
+        # After max_login_attempts, respond with a humorous message
+        funny_response = "Ha-ha! You've stumbled into our honeypot! 😜"
+        return jsonify({"message": funny_response}), 200
+
+    # Handle the login form submission
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check username and password (replace with your authentication logic)
+        if username == "your_username" and password == "your_password":
+            # Successful login, clear login attempts
+            ip_login_attempts.pop(ip_address, None)
+            # Redirect to admin panel or some other page
+            return render_template("admin_panel.html")
+
+        # If login fails, increment login attempts
+        ip_login_attempts[ip_address] = ip_login_attempts.get(ip_address, 0) + 1
+
+    # Render the admin.html template with login_attempt count
+    login_attempt = ip_login_attempts.get(ip_address, 0)
+    return render_template('admin.html', login_attempt=login_attempt)
