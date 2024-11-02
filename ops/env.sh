b2f1e76a09e53f62e8d1e9af7d0670cbeabaff92
AndrewF <andrew.figaroa@apixio.com>
2024-11-02 19:38:26 -0400

    list workdir

--- ops/env.sh
+++ ops/env.sh
@@ -4,9 +4,10 @@
 pwd
 mkdir ../local_files
 echo "${CONFIG}" | base64 -d > ${CIRCLE_WORKING_DIRECTORY}/ops/config
-chmod go-r config
-
-# set ansible_local_vars
-echo "${LOCAL_VARS}" | base64 -d > ../local_files/ansible_local_vars.yml
-echo "${INVENTORY}" | base64 -d > ../local_files/inventory
-echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> ${BASH_ENV}
+ls -all ${CIRCLE_WORKING_DIRECTORY}/ops/
+#chmod go-r config
+#
+## set ansible_local_vars
+#echo "${LOCAL_VARS}" | base64 -d > ../local_files/ansible_local_vars.yml
+#echo "${INVENTORY}" | base64 -d > ../local_files/inventory
+#echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> ${BASH_ENV}
