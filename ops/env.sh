d075f66a1ca4cc9b1e8184d714d12c5ca5e50ed9
AndrewF <andrew.figaroa@apixio.com>
2024-11-02 20:22:18 -0400

    test ansible 6

--- ops/env.sh
+++ ops/env.sh
@@ -9,7 +9,3 @@ chmod go-r ${CIRCLE_WORKING_DIRECTORY}/ops/config
 echo "${LOCAL_VARS}" | base64 -d > ../local_files/ansible_local_vars.yml
 echo "${INVENTORY}" | base64 -d > ../local_files/inventory
 echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> ${BASH_ENV}
-echo "export PYENV_ROOT=\"/virtual-python\"" >> ${BASH_ENV}
-echo "export PATH=\"\$PYENV_ROOT/bin:\$PATH\"" >> ${BASH_ENV}
-echo "export PATH=\"\$PYENV_ROOT/shims:\$PATH\"" >> ${BASH_ENV}
-echo "export PATH=\"\$PYENV_ROOT/libexec:\$PATH\"" >> ${BASH_ENV}
