8d9c0b424ed222a77fe511e8f1f5617cae8e3821
AndrewF <andrew.figaroa@apixio.com>
2026-01-21 14:49:11 -0500

    Reignite

--- ops/env.sh
+++ ops/env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
-# set config
-mkdir ../local_files
-echo "${CONFIG}" | base64 -d > ${CIRCLE_WORKING_DIRECTORY}/ops/config
-chmod go-r ${CIRCLE_WORKING_DIRECTORY}/ops/config
+mkdir -p local_files
 
-## set ansible_local_vars
-echo "${LOCAL_VARS}" | base64 -d > ../local_files/ansible_local_vars.yml
-echo "${INVENTORY}" | base64 -d > ../local_files/inventory
-echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> ${BASH_ENV}
+echo "${CONFIG}"     | base64 -d > "${CIRCLE_WORKING_DIRECTORY}/ops/config"
+chmod go-r "${CIRCLE_WORKING_DIRECTORY}/ops/config"
+
+echo "${LOCAL_VARS}" | base64 -d > "local_files/ansible_local_vars.yml"
+echo "${INVENTORY}"  | base64 -d > "local_files/inventory"
+
+# make it available immediately + for later steps
+export KUBECONFIG="${CIRCLE_WORKING_DIRECTORY}/ops/config"
+echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> "${BASH_ENV}"
