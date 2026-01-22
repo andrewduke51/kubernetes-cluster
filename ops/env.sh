af14dedc1f2f2d398006026523d641bd3a1477fa
AndrewF <andrew.figaroa@apixio.com>
2026-01-22 16:36:03 -0500

    investigate more

--- ops/env.sh
+++ ops/env.sh
@@ -3,9 +3,8 @@ set -euo pipefail
 
 mkdir -p local_files
 
-rm ${CIRCLE_WORKING_DIRECTORY}/ops/config || true
-echo "${CONFIG}"     | base64 -d > "${CIRCLE_WORKING_DIRECTORY}/ops/config"
-chmod go-r "${CIRCLE_WORKING_DIRECTORY}/ops/config"
+echo "${CONFIG}"     | base64 -d > "local_files/config"
+chmod go-r "local_files/config"
 
 echo "${LOCAL_VARS}" | base64 -d > "local_files/ansible_local_vars.yml"
 echo "${INVENTORY}"  | base64 -d > "local_files/inventory"
