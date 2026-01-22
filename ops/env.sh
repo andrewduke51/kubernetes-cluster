7dad35f803402412cd0b9b8d98c1ed10d190e36e
AndrewF <andrew.figaroa@apixio.com>
2026-01-22 17:10:01 -0500

    /tmp/yamama

--- ops/env.sh
+++ ops/env.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 mkdir -p local_files
 
-echo "${CONFIG}" | base64 -d > "/tmp/config"
-chmod go-r "/tmp/config"
+echo "${CONFIG}" | base64 -d > "local_files/config"
+chmod go-r "local_files/config"
 
 echo "${LOCAL_VARS}" | base64 -d > "local_files/ansible_local_vars.yml"
 echo "${INVENTORY}"  | base64 -d > "local_files/inventory"
 
-## make it available immediately + for later steps
-#export KUBECONFIG="${CIRCLE_WORKING_DIRECTORY}/ops/config"
-#echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> "${BASH_ENV}"
+# make it available immediately + for later steps
+export KUBECONFIG="${CIRCLE_WORKING_DIRECTORY}/ops/config"
+echo "export KUBECONFIG=${CIRCLE_WORKING_DIRECTORY}/ops/config" >> "${BASH_ENV}"
