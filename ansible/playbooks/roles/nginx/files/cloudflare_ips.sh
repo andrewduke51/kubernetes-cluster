06b8e2584053dc716d5554d66ec776cdcbff59c3
AndrewF <andrew.figaroa@apixio.com>
2026-04-14 08:33:20 -0400

    optimize app.conf

--- ansible/playbooks/roles/nginx/files/cloudflare_ips.sh
+++ ansible/playbooks/roles/nginx/files/cloudflare_ips.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
-
-set -e
+set -euo pipefail
 
 cf_ips() {
-echo "# https://www.cloudflare.com/ips"
+  echo "# https://www.cloudflare.com/ips"
 
-for type in v4 v6; do
-echo "# IP$type"
-curl -sL "https://www.cloudflare.com/ips-$type/" | sed "s|^|allow |g" | sed "s|\$|;|g"
-echo
-done
+  for type in v4 v6; do
+    echo "# IP$type"
+    curl -sL "https://www.cloudflare.com/ips-$type/" | sed 's|^|allow |; s|$|;|'
+    echo
+  done
 
-echo "#CF Update Generated at $(LC_ALL=C date)" >> /tmp/cf.log
+  echo "# CF Update Generated at $(LC_ALL=C date)"
 }
-(cf_ips && echo "#deny all; # deny all remaining ips") > /etc/nginx/allow-cloudflare-only.conf
+
+(cf_ips && echo "deny all; # deny all remaining ips") > /etc/nginx/allow-cloudflare-only.conf
